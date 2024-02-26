651a7ec5d8ac72d6be653690d5b5477d7e1251b7
CHANDRA SEKHAR REDDY UPPELA <chanduuppela@dyn-169-226-226-215.wireless.albany.edu>
2024-02-26 16:31:30 -0500

    final commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
 
 app.use(cors());
 app.use(express.static(path.join(__dirname,'public')));
