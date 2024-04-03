3efdc4e9dba1fb3d91f70adb0fde7c7937927a25
chanduupp <cuppela@albany.edu>
2024-04-03 17:41:23 -0400

    changed port number

--- server/server.js
+++ server/server.js
@@ -90,7 +90,7 @@ app.delete('/products/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log('Server listening on port ${PORT}');
 });
